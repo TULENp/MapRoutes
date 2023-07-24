90cd8e7e50e45f7a6007197a2a91208ebe59cc75
TULENp <zh_kononenko3@mail.ru>
2023-07-24 16:09:15 +0300

    Add routes table

--- src/components/RoutesTable/RoutesTable.tsx
+++ src/components/RoutesTable/RoutesTable.tsx
@@ -1,7 +1,57 @@
+import { Table } from "antd"
+import Column from "antd/es/table/Column"
 
 export function RoutesTable() {
-    
+
+    type TTableData = {
+        routeName: string,
+        routes: number[][]
+    }
+
+    const data: TTableData[] = [
+        {
+            routeName: 'Маршрут №1',
+            routes: [
+                [59.84660399, 30.29496392],
+                [59.82934196, 30.42423701],
+                [59.83567701, 30.38064206],
+            ]
+        },
+        {
+            routeName: 'Маршрут №2',
+            routes: [
+                [59.82934196, 31.42423701],
+                [59.82761295, 31.41705607],
+                [59.84660399, 31.29496392],
+            ]
+        },
+        {
+            routeName: 'Маршрут №3',
+            routes: [
+                [59.83567701, 30.38064206],
+                [59.84660399, 30.29496392],
+                [59.82761295, 30.41705607],
+            ]
+        },
+    ]
+
+    const dataSource = data.map((item, index) => ({
+        key: index,
+        routeName: item.routeName,
+        point1: item.routes[0].join(', '),
+        point2: item.routes[1].join(', '),
+        point3: item.routes[2].join(', ')
+    }))
+
+
     return (
-        <div>RoutesTable</div>
+        <Table dataSource={dataSource}
+            rowSelection={{ type: 'radio' }}
+        >
+            <Column title="Маршрут" dataIndex="routeName" key="routeName" />
+            <Column title={'Точка 1 (lat, lng)'} dataIndex="point1" key="point1" />
+            <Column title={'Точка 2 (lat, lng)'} dataIndex="point2" key="point2" />
+            <Column title={'Точка 3 (lat, lng)'} dataIndex="point3" key="point3" />
+        </Table>
     )
 }
--- src/service/osrmApi.ts
+++ src/service/osrmApi.ts
@@ -1,12 +1,13 @@
 import axios from "axios"
-import { LatLngTuple } from "leaflet";
+import { LatLngTuple } from "leaflet"
 axios.defaults.baseURL = 'http://router.project-osrm.org/route/v1/driving'
 
 // return the fastest route between locations
 export async function GetRoute(locations: LatLngTuple[]) {
 
     //TODO move up
-    const coordinates = locations.map(subArray => subArray.reverse().join(',')).join(';');
+    // transform LatLng[] to LngLat string
+    const coordinates = locations.map(subArray => subArray.reverse().join(',')).join(';')
 
     return await axios.get(`/${coordinates}?overview=full`)
         .then(response => response.data)
